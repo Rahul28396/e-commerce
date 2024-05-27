5a8191c008df601f381bed6c887a0dfdbddac03d
RahulMondal28396 <rahul.mondal28396@gmail.com>
2024-05-27 17:18:30 +0530

    Nit

--- src/app/modules/checkout/cart/cart.component.ts
+++ src/app/modules/checkout/cart/cart.component.ts
@@ -1,18 +1,28 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, DoCheck, OnChanges, OnInit, QueryList, SimpleChanges, ViewChildren } from '@angular/core';
 import { Router } from '@angular/router';
 import { CartService } from 'src/app/core/services/cart.service';
+import { CartItemComponent } from '../cart-item/cart-item.component';
 
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
   styleUrls: ['./cart.component.scss']
 })
-export class CartComponent {
+export class CartComponent implements OnInit, AfterViewInit{
 
   constructor(
     public cartService: CartService,
     private _router : Router
-  ) {
+  ) {}
+
+  @ViewChildren(CartItemComponent) cartItems!: QueryList<CartItemComponent>;
+
+  ngOnInit(): void {
+    console.log('ngOnInit called and cartItem value:', this.cartItems);
+  } 
+
+  ngAfterViewInit(): void {
+    console.log('ngAfterViewInit called and cartItem value:', this.cartItems.last);
   }
 
   clearCart() {
